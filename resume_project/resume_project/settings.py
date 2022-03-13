97df52560121ca78fde5ddaaa8ec40213dea2555
Felix Zilk <zilk.felix@gmail.com>
2022-03-13 23:26:16 +0100

    change SECRET_KEY in settings.py

--- resume_project/resume_project/settings.py
+++ resume_project/resume_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9e)h0!w%=pwc$rwv$#vr^nqox-0c-4+-((ipn^930nik&j04%y"
+SECRET_KEY = os.environ.get("SECRET_KEY", "not found")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
